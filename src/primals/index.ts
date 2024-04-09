ac6fc629caa4651b4b39a1e1a16eeec2fa66f3a3
Andy Wilson <andy@builtbypixel.com>
2024-04-09 08:40:58 +0100

    feat: add a load more elements

--- src/primals/index.ts
+++ src/primals/index.ts
@@ -3,4 +3,17 @@ export { Button } from "./button/index.mist";
 export { Divider } from "./divider/index.mist";
 export { Flex } from "./flex/index.mist";
 export { Cell, Grid } from "./grid/index.mist";
+export { Image } from "./image/index.mist";
+export { Input } from "./input/index.mist";
+export { Link } from "./link/index.mist";
+export { ListItem, OrderedList, UnorderedList } from "./list/index.mist";
 export { Ratio } from "./ratio/index.mist";
+export { Spacer } from "./spacer/index.mist";
+export { Table, Tbody, Td, Tfoot, Th, Thead, Tr } from "./table/index.mist";
+export {
+  Heading,
+  SectionHeading,
+  SubHeading,
+  SubsectionHeading,
+  Text,
+} from "./text/index.mist";
